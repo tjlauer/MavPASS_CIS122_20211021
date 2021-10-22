f2b1d5464b021ee12bd8b0a13e0cfa1e7f5c1897
Thomas J. Lauer <tlauer2@gustavus.edu>
2021-10-21 20:47:09 -0500

    Create ThomasLauer.cs

---
+++ ThomasLauer.cs
@@ -0,0 +1,17 @@
+﻿// Thomas Lauer
+// MavPASS CIS122
+// October 21, 2021
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MavPASS_CIS122_20211021
+{
+	public class ThomasLauer
+	{
+
+	}
+}
